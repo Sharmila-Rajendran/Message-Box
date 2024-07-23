2ba76f22d03a8fe850aec97343ae45d9ae139407
Sharmila-Rajendran <139245927+Sharmila-Rajendran@users.noreply.github.com>
2024-07-23 17:57:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+function Accept(){
+    const ques=document.getElementById("txt").value;
+    if(ques===''){
+        alert("Please type something");
+    }
+    else if("What is your favourite way to spend a weekend?"===ques){
+        alert("Accepted");
+    }
+    else if("Do you enjoy what you do for a living?"===ques){
+        alert("Accepted");
+    }
+    else if("What is a book that you'd recommend?"===ques){
+        alert("Accepted");
+    }
+    else if("Are you a morning person or a night owl?"===ques){
+        alert("Accepted");
+    }
+    else if("What is your dream job?"===ques){
+        alert("Accepted");
+    }
+    else if("Do you have any pets?"===ques){
+        alert("Accepted");
+    }
+    else if("What is your favourite food?"===ques){
+        alert("Accepted");
+    }
+    else if("What is your least favourite food?"===ques){
+        alert("Accepted");
+    }
+    else if("Do you have any favourite sport?"===ques){
+        alert("Accepted");
+    }
+    else if("what is your biggest fear?"===ques){
+        alert("Accepted");
+    }
+    
+}
+
+function Decline(){
+    const ques=document.getElementById("txt").value;
+    if(ques===''){
+        alert("Please type something");
+    }
+    else if("What is your favourite way to spend a weekend?"!==ques){
+        alert("Declined");
+    }
+    else if("Do you enjoy what you do for a living?"!==ques){
+        alert("Declined");
+    }
+    else if("What is a book that you'd recommend?"!==ques){
+        alert("Declined");
+    }
+    else if("Are you a morning person or a night owl?"!==ques){
+        alert("Declined");
+    }
+    else if("What is your dream job?"!==ques){
+        alert("Declined");
+    }
+    else if("Do you have any pets?"!==ques){
+        alert("Declined");
+    }
+    else if("What is your favourite food?"!==ques){
+        alert("Declined");
+    }
+    else if("What is your least favourite food?"!==ques){
+        alert("Declined");
+    }
+    else if("Do you have any favourite sport?"!==ques){
+        alert("Declined");
+    }
+    else if("what is your biggest fear?"!==ques){
+        alert("Declined");
+    }
+    
+
+}
